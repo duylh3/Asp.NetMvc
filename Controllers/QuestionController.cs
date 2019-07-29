cebb7e573270ff9f78e97d181bfbb1569a458144
Bellamy <duyitdotcom@gmail.com>
2019-07-29 22:48:40 +0700

    first source

---
+++ Controllers/QuestionController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ideal.Repository;
+
+namespace ideal.Controllers
+{
+    public class QuestionController : Controller
+    {
+        private QuestionRepository _re = new QuestionRepository();
+        //    GET: QA
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CauHoi()
+        {
+            return View();
+        }
+
+        public JsonResult GetData(int page, int pageSize, string cate)
+        {
+            var data = _re.ListQuestion(page, pageSize, cate);
+
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Scripts/app/QuestionJS.js
@@ -0,0 +1,89 @@
+﻿$(document).ready(function(){
+    $("#Salary").click(function (e) {
+        e.preventDefault();
+        var cate = $(this).attr("id");
+        var template = $("#table-template").html();
+        var render = "";
+        $.ajax({
+            type: "GET",
+            data: {
+                cate: cate,
+                page: 1,
+                pageSize: 10
+            },
+            url: "/Question/GetData",
+            dataType: "json",
+            success: function (response) {
+                if (response.Results.length === 0) {
+                    $("#tbl-content").html("");
+                    $("#tbl-content").append($("<tr>").append($("<td>").attr("colspan", 8).html("There is no data to display!!").addClass("text-center")));
+                }
+                $.each(response.Results, function (i, item) {
+                    render += Mustache.render(template, {
+                        Sequence: i + 1,
+                        Id: item.Id,
+                        Question: item.Question,
+                        Category: item.Category,
+                        CreatedBy: item.CreatedBy
+                    });
+
+                    if (render != "") {
+                        $("#tbl-content").html(render);
+                        $("#lblTotalRecord").html(response.RowCount);
+                    }
+                });
+            },
+            error: function (status) {
+                console.log(status);
+            }
+        });
+    });
+
+    function wrapPaging(recordCount, callBack, changePageSize) {
+        var totalSize = Math.ceil(recordCount / common.configs.pageSize);
+        //Unbind pagination if it existed or click change pagesize
+        if ($("#paginationUl a").length === 0 || changePageSize === true) {
+            $("#paginationUl").empty();
+            $("#paginationUl").removeData("twbs-pagination");
+            $("#paginationUl").unbind("page");
+        }
+        //Bind Pagination Event
+        $("#paginationUl").twbsPagination({
+            totalPages: totalSize,
+            visiblePages: 7,
+            onPageClick: function (e, p) {
+                common.configs.pageIndex = p;
+                setTimeout(callBack(), 200);
+            }
+        });
+    };
+
+    $("body").on("click", ".btn-edit", function (e) {
+        e.preventDefault();
+        var that = $(this).data("id");
+
+        alert(that);
+        $.ajax({
+            type: "GET",
+            url: "/Question/GetQuesById",
+            data: { id: that },
+            dataType: "json",
+            beforeSend: function () {
+                common.startLoading();
+            },
+            success: function (response) {
+                var data = response;
+                resetFormDetail(data);
+                $('#modalDetail').modal('show');
+                common.stopLoading();
+
+            },
+            error: function (status) {
+                console.log(status);
+                common.notify("Hyosung Motor", 'Có lỗi xảy ra', 'gray error');
+                common.stopLoading();
+            }
+        });
+    });
+
+});
