18a6aa8f0962e7b7d264543e83481c9202f1ba1e
Miyake <duyitdotcom@gmail.com>
2019-07-30 17:18:21 +0700

    Add files via upload

---
+++ DocumentController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ideal.Repositories;
+
+namespace ideal.Controllers
+{
+    public class DocumentController : Controller
+    {
+        DocumentRepository _re = new DocumentRepository();
+
+        // GET: Document
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ThamKhao()
+        {
+            return View();
+        }
+
+        public JsonResult GetData(string cate)
+        {
+            var data = _re.ListDocument(cate);
+
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetDocumentDetail(int id)
+        {
+            var data = _re.GetDocumentDetail(id);
+
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ documentjs.js
@@ -0,0 +1,85 @@
+﻿$(document).ready(function () {
+    $("#Law").click(function (e) {
+        e.preventDefault();
+        var cate = $(this).attr("id");
+        LoadData(cate);
+    });
+
+    $("#Labor").click(function (e) {
+        e.preventDefault();
+        var cate = $(this).attr("id");
+        LoadData(cate);
+    });
+
+    function LoadData(cate) {
+        var template = $("#table-template").html();
+        var render = "";
+        $.ajax({
+            type: "GET",
+            data: {
+                //   pageSize: 1,
+                //   page: 1,
+                cate: cate
+            },
+            url: "/Document/GetData",
+            dataType: "json",
+            success: function (response) {
+                if (response.Results.length === 0) {
+                    $("#tbl-content").html("");
+                    $("#tbl-content").append($("<tr>").append($("<td>").attr("colspan", 8).html("Không có câu hỏi liên quan!!").addClass("text-center")));
+                }
+
+                $.each(response.Results, function (i, item) {
+                    render += Mustache.render(template, {
+                        Sequence: i + 1,
+                        Id: item.Id,
+                        DocumentName: item.DocumentName,
+                        Category: item.Category,
+                        CreatedBy: item.CreatedBy,
+                    });
+                    if (render != "") {
+                        $("#tbl-content").html(render);
+                        $("#lblTotalRecord").html(response.RowCount);
+                    }
+                })
+            },
+            error: function (status) {
+                console.log(status);
+            }
+        });
+    }
+
+
+    //---------------------- Cau Tra loi ---------------------------------------------//
+    $("body").on("click", ".btn-edit", function (e) {
+        e.preventDefault();
+        var that = $(this).data("id");
+        $('#modalDetail').modal('show');
+        $.ajax({
+            type: "GET",
+            url: "/Document/GetDocumentDetail",
+            data: { id: that },
+            dataType: "json",
+            success: function (response) {
+                
+                var data = response;
+                console.log(data);
+                var document = '<h5 class="modal-title" id="modalTitle">' + data.DocumentName + '</h5>';
+              
+
+                $("#question").append(document);
+      
+
+                $('#modalDetail').modal('show');
+            },
+            error: function (status) {
+                console.log(status);
+            }
+        });
+    });
+    //---------------------- End ---------------------------------------------//
+    function ClearData() {
+        $("#question").text("");
+        $("#content").text("");
+    }
+});
